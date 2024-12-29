e0a21a99c00df6ca604bd60b70b54c5efde24482
FOTH <1991808595@qq.com>
2024-12-29 20:10:07 +0800

    add css and ts

--- WebGIS/settings.py
+++ WebGIS/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    # BASE_DIR / "Map"
 ]
 #
 # Default primary key field type
---
+++ static/map.ts
@@ -0,0 +1,8 @@
+import * as L from "leaflet";
+
+const osm = "https://www.openstreetmap.org/copyright";
+const copy = `© <a href='${osm}'>OpenStreetMap</a>`;
+const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const layer = L.tileLayer(url, { attribution: copy });
+const map = L.map("map", { layers: [layer] });
+map.fitWorld();
